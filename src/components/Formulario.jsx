a1e8558a6462b190e636d8f7f0aadb35068fdd82
Luis Puello <luiscapupa@gmail.com>
2023-05-20 09:35:17 -0500

    Initial Interface

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,8 +1,35 @@
-import React from 'react'
-
+import React, {useState, useEffect} from 'react'
+import { db } from '../firebase'
+import { collection, doc, addDoc, onSnapshot, deleteDoc, updateDoc } from 'firebase/firestore'
 function Formulario() {
+    const [nombreLibro, setNombreLibro] = useState('');
+    const [nombreAutor, setNombreAutor] = useState('');
   return (
-    <div>Formulario</div>
+    <div className='container mt-5'>
+        <h1 className='text-center'>CRUD DE LIBROS</h1>
+        <hr />
+        <div className="row">
+            <div className="col-8">
+                <h4 className='text-center'>Listado de Libros</h4>
+                <ul className="list-group">
+                    <li className="list-group-item">
+                        <span className="lead">Test</span>
+                        <button className="btn btn-danger btn-sm float-end mx-2">Eliminar</button>
+                        <button className="btn btn-warning btn-sm float-end">Editar</button>
+                    </li>
+                </ul>
+            </div>
+            <div className="col-4">
+                <h4 className="text-center">AGREGAR LIBROS</h4>
+                <form >
+                    <input type="text" className="form-control mb-2" placeholder='Ingrese nombre del libro' />
+                    <input type="text" className="form-control mb-2" placeholder='Ingrese nombre del autor' />
+                    <button className="btn btn-primary btn-block">Agregar</button>
+                </form>
+            </div>
+        </div>
+        
+    </div>
   )
 }
 
